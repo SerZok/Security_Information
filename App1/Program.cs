559ac009945bc359b6b5b9aedf140b012045e172
SerZok <SerZok.021@gmail.com>
2024-10-22 10:45:48 +0300

    Add project files.

---
+++ App1/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+class SubstitutionCipher
+{
+    // Алфавит
+    static readonly char[] alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+
+    //Таблица подстановки
+    static readonly Dictionary<char, char> substitutionTable = new Dictionary<char, char>
+    {
+        {'А', 'Г'}, {'Б', 'Ж'}, {'В', 'К'}, {'Г', 'Л'}, {'Д', 'Н'}, {'Е', 'Р'},
+        {'Ё', 'С'}, {'Ж', 'Т'}, {'З', 'У'}, {'И', 'Ф'}, {'Й', 'Х'}, {'К', 'Ц'},
+        {'Л', 'Ч'}, {'М', 'Ш'}, {'Н', 'Щ'}, {'О', 'Ъ'}, {'П', 'Ы'}, {'Р', 'Ь'},
+        {'С', 'Э'}, {'Т', 'Ю'}, {'У', 'Я'}, {'Ф', 'А'}, {'Х', 'Б'}, {'Ц', 'В'},
+        {'Ч', 'Д'}, {'Ш', 'Е'}, {'Щ', 'Ё'}, {'Ъ', 'Ж'}, {'Ы', 'З'}, {'Ь', 'И'},
+        {'Э', 'Й'}, {'Ю', 'М'}, {'Я', 'О'}
+    };
+
+    static void Main()
+    {
+        Console.WriteLine("Введите текст для шифрования (только русские буквы):");
+        string text = Console.ReadLine().ToUpper();
+
+        string encryptedText = Encrypt(text);
+        string decryptedText = Decrypt(encryptedText);
+
+        Console.WriteLine($"Зашифрованный текст: {encryptedText}");
+        Console.WriteLine($"Расшифрованный текст: {decryptedText}");
+    }
+
+    // Функция шифрования
+    static string Encrypt(string plainText)
+    {
+        string cipherText = "";
+        foreach (char ch in plainText)
+        {
+            if (substitutionTable.ContainsKey(ch))
+                cipherText += substitutionTable[ch];
+            else
+                cipherText += ch;
+        }
+        return cipherText;
+    }
+
+    // Функция дешифрования
+    static string Decrypt(string cipherText)
+    {
+        string plainText = "";
+        foreach (char ch in cipherText)
+        {
+            char originalChar = GetOriginalChar(ch);
+            plainText += originalChar;
+        }
+        return plainText;
+    }
+
+    // Поиск оригинального символа для дешифрования
+    static char GetOriginalChar(char cipherChar)
+    {
+        foreach (var pair in substitutionTable)
+        {
+            if (pair.Value == cipherChar)
+                return pair.Key;
+        }
+        return cipherChar; //Если не нашли то просто возварщаем
+    }
+}
---
+++ App2/Program.cs
@@ -0,0 +1,70 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        Security security = new Security();
+        string text;
+
+        text = Security.Input();
+        int[] ints = new int[10]{ 1, 2, 3, 4, 5, 6, 7 , 8, 9, 0};
+
+        //Console.WriteLine(text);
+        security.Coding(text, ints);
+
+
+    }
+}
+//{А,Б,..,Я,<пробел>} 17
+
+class Security {
+    public static string Input()
+    {
+        return Console.ReadLine();
+    }
+
+    public void Coding(string ioText, int[]Cn)
+    {
+        blocks = Blocking(ioText);
+        foreach(string block in blocks)
+        {
+            Console.WriteLine($"До кодирования: {block}");
+
+            char[] chars = block.ToCharArray();
+
+            for(int i=0; i<block.Length; i++)
+            {
+                chars[i] = chars[Cn[i]];
+            }
+
+            Console.WriteLine($"После кодирования: {chars}");
+
+        }
+    }
+
+    private List<string> blocks = new List<string>();
+    public int[] Cn;
+
+    private List<string> Blocking(string ioText)
+    {
+
+        for (int i = 0; i < ioText.Length; i += 10)
+        {
+            // Извлекаем подстроку длиной до 10 символов
+            string block = ioText.Substring(i, Math.Min(10, ioText.Length - i));
+
+            // Если блок меньше 10 символов, дополняем пробелами
+            while (block.Length < 10)
+            {
+                block += '*';
+            }
+
+            blocks.Add(block);
+        }
+
+        return blocks;
+    }
+
+
+}
+
